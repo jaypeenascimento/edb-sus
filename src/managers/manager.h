f2f589b55ad1554d4f26ce633f5bcbaba93a95f2
Pedro <joao.snascimento@stone.com.br>
2025-07-01 19:26:56 -0300

    feat: add manager context structure

---
+++ src/managers/manager.h
@@ -0,0 +1,10 @@
+#ifndef MANAGER_H
+#define MANAGER_H
+
+typedef struct ManagerContext {
+  // Temporarily empty struct to represent a manager context.
+} ManagerContext;
+
+ManagerContext *new_manager_context();
+
+#endif
