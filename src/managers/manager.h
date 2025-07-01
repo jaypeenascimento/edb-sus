24d6e1519319074c298348e035f8a655ebc72d66
Pedro <joao.snascimento@stone.com.br>
2025-07-01 19:30:13 -0300

    chore: improve comments

--- src/managers/manager.h
+++ src/managers/manager.h
@@ -3,6 +3,7 @@
 
 typedef struct ManagerContext {
   // Temporarily empty struct to represent a manager context.
+  // Add any necessary fields for the managers to function here later.
 } ManagerContext;
 
 ManagerContext *new_manager_context();
