81fe93771288237224f016445e025f6f72864b02
Pedro <joao.snascimento@stone.com.br>
2025-07-01 18:20:35 -0300

    feat: add deque enqueue functions

--- src/structures/deque.c
+++ src/structures/deque.c
@@ -21,3 +21,41 @@ Deque *deque_create() {
   deque->back = NULL;
   return deque;
 }
+
+void deque_enqueuefront(Deque *deque, void *data) {
+  DequeNode *node = deque_createnode(data);
+  if (!node) {
+    // FORCEFULL CRASH PROGRAM IF NO MEMORY.
+    return exit(1);
+  }
+
+  if (deque->front == NULL) {
+    // Deque is empty.
+    deque->front = node;
+    deque->back = node;
+  } else {
+    // Deque is non-empty.
+    node->prev = deque->front;
+    deque->front->next = node;
+    deque->front = node;
+  }
+}
+
+void deque_enqueueback(Deque *deque, void *data) {
+  DequeNode *node = deque_createnode(data);
+  if (!node) {
+    // FORCEFULL CRASH PROGRAM IF NO MEMORY.
+    return exit(1);
+  }
+
+  if (deque->front == NULL) {
+    // Deque is empty.
+    deque->front = node;
+    deque->back = node;
+  } else {
+    // Deque is non-empty.
+    node->next = deque->back;
+    deque->back->prev = node;
+    deque->back = node;
+  }
+}
