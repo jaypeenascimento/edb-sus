f4b0cd891226c47a87014b5c075d944a74e51ba5
Pedro <joao.snascimento@stone.com.br>
2025-07-01 20:55:47 -0300

    feat: add wailistmanager to manager context

--- src/managers/manager.c
+++ src/managers/manager.c
@@ -1,11 +1,13 @@
 #include "manager.h"
 #include <stdlib.h>
 
-ManagerContext *new_manager_context() {
+ManagerContext *new_manager_context(WaitlistManager *wl) {
   ManagerContext *ctx = malloc(sizeof(ManagerContext));
   if (ctx == NULL) {
     return NULL;
   }
+
+  ctx->waitlist_manager = wl;
 
   return ctx;
 }
