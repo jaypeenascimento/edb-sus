e34db36902d589bf9d2a88a441878164a2ada4da
Pedro <joao.snascimento@stone.com.br>
2025-07-04 20:34:11 -0300

    feat: add bedlist to main.c

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
-#include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "src/extensions/filereader.h"
 #include "src/extensions/logger.h"
+#include "src/managers/bedlist_manager.h"
 #include "src/managers/manager.h"
-#include "src/managers/waitlist_manager.h"
 #include "src/managers/patientdata_manager.h"
+#include "src/managers/waitlist_manager.h"
 #include "src/structures/hashmap.h"
-#include "src/structures/stack.h"
 
 int main() {
   srand(time(NULL));
@@ -17,6 +17,7 @@ int main() {
   loadPatients(hashmap, "./data/in/pacientes.csv");
 
   WaitlistManager *wl = newWaitlistManager();
+  BedlistManager *bl = NewBedlistManager();
   ManagerContext *ctx = newManagerContext(wl);
 
   while (1) {
@@ -24,19 +25,22 @@ int main() {
     Patient *randomPatient = patientDataManagerGetRandomPatient(hashmap);
     if (randomPatient) {
       waitlistManagerInsertPatient(wl, randomPatient);
-      logMessage("[INFO] Paciente transferido do hashmap para o deque: %s", randomPatient->name);
+      logMessage("[INFO] Paciente transferido do hashmap para o deque: %s",
+                 randomPatient->name);
       patientRemove(hashmap, randomPatient->id);
     } else {
       logMessage("[INFO] Não há mais pacientes para transferir do hashmap.");
     }
 
     bool wlreturn = waitlistManagerProcess(wl, ctx);
+    bool blreturn = BedlistManagerProcess(bl, ctx);
 
     sleep(2);
 
     // Exit loop if all managers return false.
-    if (!wlreturn) {
-      logMessage("[INFO] Finalizando ciclos dado que os gerenciadores estão todos ociosos");
+    if (!(wlreturn || blreturn)) {
+      logMessage("[INFO] Finalizando ciclos dado que os gerenciadores estão "
+                 "todos ociosos");
       break;
     }
   }
